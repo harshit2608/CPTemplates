d8d4fe04f062946284fd90a65bc80a37bcdb160b
HARSHIT BARGUJAR <42277796+harshit2608@users.noreply.github.com>
2021-09-26 21:57:03 +0530

    Update Codechef.cpp

--- Codechef.cpp
+++ Codechef.cpp
@@ -1,50 +1,67 @@
 #include <bits/stdc++.h>
+using namespace std;
 
+#define HAR_HAR_MAHADEV int main()
+#define PI 3.1415926535897932384626
 #define mod int(1e9 + 7)
 #define ll long long
 #define ld long double
 #define ull unsigned long long
-#define vci vector<int>
-#define vcl vector<ll>
-#define umi unordered_map<int, int>
-#define uml unordered_map<ll, ll>
 #define F first
 #define S second
 #define PB push_back
 #define DB pop_back
 #define MP make_pair
-#define tc           \
-    int t;           \
-    scanf("%d", &t); \
+#define rep(i, a, b) for (ll i = a; i < b; ++i)
+#define repi(i, a, b) for (ll i = b - 1; i >= a; --i)
+#define tc    \
+    int t;    \
+    cin >> t; \
     while (t--)
+#define fastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0)
 
-using namespace std;
+// typedef vector<int> vci;
+// typedef vector<ll> vcl;
+// typedef unordered_map<int, int> umi;
+// typedef unordered_map<ll, ll> uml;
 
-void fastIO()
+ll fastpow(ll x, ull n, ll m = mod)
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+    ll res = 1;
+    x = x % m;
+    if (x == 0 || x == 1)
+    {
+        return x;
+    }
+    while (n > 0)
+    {
+        if (n & 1)
+        {
+            res = (res * x) % m;
+        }
+        x = (x * x) % m;
+        n = n >> 1;
+    }
+    return res;
 }
 
-void solve(int a[], int n)
+void solve()
 {
-    int ans = 0;
-    printf("%d\n", ans);
+
 }
-int main()
+
+HAR_HAR_MAHADEV
 {
-    fastIO();
+    fastIO;
+#ifndef ONLINE_JUDGE
+    cerr << "HEllo";
+#endif
     tc
     {
-        int ans = 0, n;
-        scanf("%d", &n);
-        int a[n];
-        for (int i = 0; i < n; ++i)
-        {
-            scanf("%d", &a[i]);
-        }
-        solve(a, n);
+        solve();
     }
     return 0;
 }
